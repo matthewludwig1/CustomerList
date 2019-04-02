98b3dd59878d748082e006d6fbd20a0ace639e9e
matthewludwig1 <36999944+matthewludwig1@users.noreply.github.com>
2019-04-02 19:17:44 -0400

    Update CustomerList.java

--- src/customerlist/CustomerList.java
+++ src/customerlist/CustomerList.java
@@ -1,6 +1,7 @@
 /*
    Matt Ludwig
    2 April 2019
+   This program writes customer data to an external text file
  */
 
 package customerlist;
@@ -23,28 +24,48 @@ public class CustomerList {
        
        BufferedReader readFile = new BufferedReader(new FileReader("customers.txt"));
        System.out.println(readFile.readLine());
+       String line = "";
+           while((line = readFile.readLine()) != null){ //EOF
+           System.out.println(line);
+        }
+       
        readFile.close();
-       numCustomers = Integer.parseInt(JOptionPane.showInputDialog("How Many Customers do"
-               + " you want to enter?"));
-       PrintWriter fileOut = new PrintWriter(new FileWriter( 
-        "customers.txt", true));
+       try {
+            numCustomers = Integer.parseInt(JOptionPane.showInputDialog("How Many Customers do"
+                + " you want to enter?"));
+                PrintWriter fileOut = new PrintWriter(new FileWriter( 
+                "customers.txt", true));
        
-       for (int i = 0; i < numCustomers; i++) {
-            name = JOptionPane.showInputDialog("Enter the name of the customer:");
-            province = JOptionPane.showInputDialog("Enter the customer's home province:");
-            city = JOptionPane.showInputDialog("Enter the customer's city");
-            address = JOptionPane.showInputDialog("Enter the customer's address:");
-            postalCode = JOptionPane.showInputDialog("Enter the customer's postal code:");
-            
-            fileOut.println();
-            fileOut.println("Name: "+ name);
-            fileOut.println();
-            fileOut.println("Province: "+ province);
-            fileOut.println();
-            fileOut.println("City: "+ city);
-            fileOut.println();
-       
+            for (int i = 0; i < numCustomers; i++) { //EOF
+                int custNum = i +1;
+                name = JOptionPane.showInputDialog("Enter the name of customer " + custNum +":");
+                province = JOptionPane.showInputDialog("Enter customer "+ custNum + "'s "+ "home province:");
+                city = JOptionPane.showInputDialog("Enter the customer's city");
+                address = JOptionPane.showInputDialog("Enter the customer's address:");
+                postalCode = JOptionPane.showInputDialog("Enter the customer's postal code:");
+              
+              if (name == null || province == null || city == null || address == null || postalCode == null) {
+                  break;
+              }
+           
+                fileOut.println();
+                fileOut.println();
+                fileOut.append("Name: "+ name);
+                fileOut.println();
+                fileOut.append("Province: "+ province);
+                fileOut.println();
+                fileOut.append("City: "+ city);
+                fileOut.println();
+                fileOut.append("Address: "+ address);
+                fileOut.println();
+                fileOut.append("Postal Code: "+ postalCode);
+          
+            }
+            fileOut.close();
        }
+       catch (NumberFormatException exc) { 
+           System.out.println(exc); 
+       } 
     }
     
 }
